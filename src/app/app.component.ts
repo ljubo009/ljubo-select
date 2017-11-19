4a2e2538b08b0877fe580f2cae65d5d6c4c69a59
Ljubo <ljubomir.susnja@gmail.com>
2017-11-19 16:12:01 +0100

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private lista = [{naziv: 'test 1', id: 1}, {naziv: 'test 2', id: 2}, {naziv: 'test 3', id: 3}];
+  private selData = {naziv: 'test 1', id: 1};
   title = 'app';
+  constructor() {
+
+  }
+  selectI(evt) {
+    const that = this;
+    console.log('ODABRAN JE...', evt, this.selData);
+  }
+  restPoziv() {
+    const that = this;
+    return function (){
+      return new Promise((resolve, reject) => {
+        setTimeout( () => {
+          console.log(that.title);
+          resolve([{naziv: 'test 4', id: 4}, {naziv: 'test 5', id: 5}]);
+        }, 1000);
+      });
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LjuboSelectModule} from './modules/ljubo-select/ljubo-select.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    LjuboSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
